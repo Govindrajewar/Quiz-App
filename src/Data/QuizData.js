250734176130e104ebd2e729ef130b1e72864e1c
Govind Rajewar <abhirajewar87@gmail.com>
2024-06-20 14:31:21 +0530

    Added logo and title & Updated some styling

--- src/Data/QuizData.js
+++ src/Data/QuizData.js
@@ -26,178 +26,178 @@ export const QuizData = [
     answer: "B) render()",
     correctAnswer: 2,
   },
-  // {
-  //   question: "Total ways of defining variables in ES6 is?",
-  //   options: ["A) One", "B) Two", "C) Three", "D) Four"],
-  //   answer: "C) Three",
-  //   correctAnswer: 3,
-  // },
-  // {
-  //   question:
-  //     "Using which of the following command can prevent default behaviour in react?",
-  //   options: [
-  //     "A) preventDefault()",
-  //     "B) stopPropagation()",
-  //     "C) stopDefault()",
-  //     "D) preventBehavior()",
-  //   ],
-  //   answer: "A) preventDefault()",
-  //   correctAnswer: 1,
-  // },
-  // {
-  //   question: "What are arbitrary inputs of components in react also known as?",
-  //   options: ["A) Keys", "B) Props", "C) Elements", "D) States"],
-  //   answer: "B) Props",
-  //   correctAnswer: 2,
-  // },
-  // {
-  //   question:
-  //     "What happens if you render an input element with disabled = {false}?",
-  //   options: [
-  //     "A) It will be rendered as enabled",
-  //     "B) It will be rendered as disabled",
-  //     "C) An error will be thrown",
-  //     "D) Nothing will happen",
-  //   ],
-  //   answer: "A) It will be rendered as enabled",
-  //   correctAnswer: 1,
-  // },
-  // {
-  //   question: "What is Babel?",
-  //   options: [
-  //     "A) JavaScript compiler",
-  //     "B) JavaScript transpiler",
-  //     "C) JavaScript interpreter",
-  //     "D) JavaScript linter",
-  //   ],
-  //   answer: "A) JavaScript compiler",
-  //   correctAnswer: 1,
-  // },
-  // {
-  //   question: "What is ReactJS mainly used for building?",
-  //   options: [
-  //     "A) Servers",
-  //     "B) Databases",
-  //     "C) User interfaces",
-  //     "D) Mobile apps",
-  //   ],
-  //   answer: "C) User interfaces",
-  //   correctAnswer: 3,
-  // },
-  // {
-  //   question: "What is ReactJS?",
-  //   options: [
-  //     "A) A server-side framework",
-  //     "B) A user-interface framework",
-  //     "C) A database",
-  //     "D) A mobile app framework",
-  //   ],
-  //   answer: "B) A user-interface framework",
-  //   correctAnswer: 2,
-  // },
-  // {
-  //   question: "What is the functionality of a “webpack” command?",
-  //   options: [
-  //     "A) Module bundler",
-  //     "B) Package manager",
-  //     "C) Task runner",
-  //     "D) Code formatter",
-  //   ],
-  //   answer: "A) Module bundler",
-  //   correctAnswer: 1,
-  // },
-  // {
-  //   question: "What is used to handle code-splitting?",
-  //   options: [
-  //     "A) React.lazy",
-  //     "B) React.split",
-  //     "C) React.fragment",
-  //     "D) React.codeSplit",
-  //   ],
-  //   answer: "A) React.lazy",
-  //   correctAnswer: 1,
-  // },
-  // {
-  //   question: "When is useReducer used over useState in React component?",
-  //   options: [
-  //     "A) When we want to replace redux",
-  //     "B) When we have a single state",
-  //     "C) When we have no state",
-  //     "D) When we want to optimize performance",
-  //   ],
-  //   answer: "A) When we want to replace redux",
-  //   correctAnswer: 1,
-  // },
-  // {
-  //   question:
-  //     "State whether true or false: React merges the objects you provide into the current state using setState().",
-  //   options: ["A) True", "B) False"],
-  //   answer: "A) True",
-  //   correctAnswer: 1,
-  // },
-  // {
-  //   question: "Which company developed ReactJS?",
-  //   options: ["A) Google", "B) Facebook", "C) Microsoft", "D) Apple"],
-  //   answer: "B) Facebook",
-  //   correctAnswer: 2,
-  // },
-  // {
-  //   question: "Which of the following are two ways to handle data in react?",
-  //   options: [
-  //     "A) Services & Components",
-  //     "B) State & Props",
-  //     "C) State & Services",
-  //     "D) Props & Services",
-  //   ],
-  //   answer: "B) State & Props",
-  //   correctAnswer: 2,
-  // },
-  // {
-  //   question:
-  //     "Which of the following function is used to change the state of react component?",
-  //   options: [
-  //     "A) this.setState()",
-  //     "B) this.changeState()",
-  //     "C) this.updateState()",
-  //     "D) this.modifyState()",
-  //   ],
-  //   answer: "A) this.setState()",
-  //   correctAnswer: 1,
-  // },
-  // {
-  //   question:
-  //     "Which of the following is used in React.js to increase performance?",
-  //   options: [
-  //     "A) Virtual DOM",
-  //     "B) Real DOM",
-  //     "C) Both A and B",
-  //     "D) None of the above",
-  //   ],
-  //   answer: "A) Virtual DOM",
-  //   correctAnswer: 1,
-  // },
-  // {
-  //   question:
-  //     "Which of the following is used to access a function fetch() from h1 element in JSX?",
-  //   options: [
-  //     "A) <h1>{fetch}</h1>",
-  //     "B) <h1>${fetch()}</h1>",
-  //     "C) <h1>${fetch}</h1>",
-  //     "D) <h1>{fetch()}</h1>",
-  //   ],
-  //   answer: "D) <h1>{fetch()}</h1>",
-  //   correctAnswer: 4,
-  // },
-  // {
-  //   question:
-  //     "Identify the command used to create a react app",
-  //   options: [
-  //     "A) npm install create-react-app",
-  //     "B) npm install -g create-react-app",
-  //     "C) install -g create-react-app",
-  //     "D) None of the above"
-  //   ],
-  //   answer: "A) npm install create-react-app",
-  //   correctAnswer: 1,
-  // },
+  {
+    question: "Total ways of defining variables in ES6 is?",
+    options: ["A) One", "B) Two", "C) Three", "D) Four"],
+    answer: "C) Three",
+    correctAnswer: 3,
+  },
+  {
+    question:
+      "Using which of the following command can prevent default behaviour in react?",
+    options: [
+      "A) preventDefault()",
+      "B) stopPropagation()",
+      "C) stopDefault()",
+      "D) preventBehavior()",
+    ],
+    answer: "A) preventDefault()",
+    correctAnswer: 1,
+  },
+  {
+    question: "What are arbitrary inputs of components in react also known as?",
+    options: ["A) Keys", "B) Props", "C) Elements", "D) States"],
+    answer: "B) Props",
+    correctAnswer: 2,
+  },
+  {
+    question:
+      "What happens if you render an input element with disabled = {false}?",
+    options: [
+      "A) It will be rendered as enabled",
+      "B) It will be rendered as disabled",
+      "C) An error will be thrown",
+      "D) Nothing will happen",
+    ],
+    answer: "A) It will be rendered as enabled",
+    correctAnswer: 1,
+  },
+  {
+    question: "What is Babel?",
+    options: [
+      "A) JavaScript compiler",
+      "B) JavaScript transpiler",
+      "C) JavaScript interpreter",
+      "D) JavaScript linter",
+    ],
+    answer: "A) JavaScript compiler",
+    correctAnswer: 1,
+  },
+  {
+    question: "What is ReactJS mainly used for building?",
+    options: [
+      "A) Servers",
+      "B) Databases",
+      "C) User interfaces",
+      "D) Mobile apps",
+    ],
+    answer: "C) User interfaces",
+    correctAnswer: 3,
+  },
+  {
+    question: "What is ReactJS?",
+    options: [
+      "A) A server-side framework",
+      "B) A user-interface framework",
+      "C) A database",
+      "D) A mobile app framework",
+    ],
+    answer: "B) A user-interface framework",
+    correctAnswer: 2,
+  },
+  {
+    question: "What is the functionality of a “webpack” command?",
+    options: [
+      "A) Module bundler",
+      "B) Package manager",
+      "C) Task runner",
+      "D) Code formatter",
+    ],
+    answer: "A) Module bundler",
+    correctAnswer: 1,
+  },
+  {
+    question: "What is used to handle code-splitting?",
+    options: [
+      "A) React.lazy",
+      "B) React.split",
+      "C) React.fragment",
+      "D) React.codeSplit",
+    ],
+    answer: "A) React.lazy",
+    correctAnswer: 1,
+  },
+  {
+    question: "When is useReducer used over useState in React component?",
+    options: [
+      "A) When we want to replace redux",
+      "B) When we have a single state",
+      "C) When we have no state",
+      "D) When we want to optimize performance",
+    ],
+    answer: "A) When we want to replace redux",
+    correctAnswer: 1,
+  },
+  {
+    question:
+      "State whether true or false: React merges the objects you provide into the current state using setState().",
+    options: ["A) True", "B) False"],
+    answer: "A) True",
+    correctAnswer: 1,
+  },
+  {
+    question: "Which company developed ReactJS?",
+    options: ["A) Google", "B) Facebook", "C) Microsoft", "D) Apple"],
+    answer: "B) Facebook",
+    correctAnswer: 2,
+  },
+  {
+    question: "Which of the following are two ways to handle data in react?",
+    options: [
+      "A) Services & Components",
+      "B) State & Props",
+      "C) State & Services",
+      "D) Props & Services",
+    ],
+    answer: "B) State & Props",
+    correctAnswer: 2,
+  },
+  {
+    question:
+      "Which of the following function is used to change the state of react component?",
+    options: [
+      "A) this.setState()",
+      "B) this.changeState()",
+      "C) this.updateState()",
+      "D) this.modifyState()",
+    ],
+    answer: "A) this.setState()",
+    correctAnswer: 1,
+  },
+  {
+    question:
+      "Which of the following is used in React.js to increase performance?",
+    options: [
+      "A) Virtual DOM",
+      "B) Real DOM",
+      "C) Both A and B",
+      "D) None of the above",
+    ],
+    answer: "A) Virtual DOM",
+    correctAnswer: 1,
+  },
+  {
+    question:
+      "Which of the following is used to access a function fetch() from h1 element in JSX?",
+    options: [
+      "A) <h1>{fetch}</h1>",
+      "B) <h1>${fetch()}</h1>",
+      "C) <h1>${fetch}</h1>",
+      "D) <h1>{fetch()}</h1>",
+    ],
+    answer: "D) <h1>{fetch()}</h1>",
+    correctAnswer: 4,
+  },
+  {
+    question:
+      "Identify the command used to create a react app",
+    options: [
+      "A) npm install create-react-app",
+      "B) npm install -g create-react-app",
+      "C) install -g create-react-app",
+      "D) None of the above"
+    ],
+    answer: "A) npm install create-react-app",
+    correctAnswer: 1,
+  },
 ];
